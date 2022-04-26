7b09a3881e318453488f3326813181d778fcbbf7
josealonso <jose.alonso.programmer@gmail.com>
2022-04-27 01:45:31 +0200

    Put my data in the corresponding json file

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,104 +1,104 @@
 let resumeData = {
-  "imagebaseurl":"https://techonoeticsclub.github.io/",
-  "name": "John Doe",
-  "role": "Frontend Developer",
-  "linkedinId":"Your LinkedIn Id",
+  "imagebaseurl": "https://techonoeticsclub.github.io/",
+  "name": "José Ramón (JR)",
+  "role": "Solidity and DApp Developer",
+  "linkedinId": "Your LinkedIn Id",
   "skypeid": "Your skypeid",
-  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-  "socialLinks":[
-      {
-        "name":"linkedin",
-        "url":"https://www.linkedin.com/",
-        "className":"fa fa-linkedin"
-      },
-      {
-        "name":"github",
-        "url":"http://github.com/",
-        "className":"fa fa-github"
-      },
-      {
-        "name":"skype",
-        "url":"http://skype.com",
-        "className":"fa fa-skype"
-      }
-    ],
-  "aboutme":"I have finished my B.E. in Computer Science and have been working in industry for past few years.",
-  "address":"India",
-  "website":"https://www.codeclassifiers.com",
-  "education":[
+  "roleDescription": "I specially like Computer Science subjects when I was in college. I learnt GNU/Linux by myself. I started my career as a C programmer and Linux admin. Later I joint a bootcamp to be a web developer. At work I used Java technologies(Spring Boot, Hibernate), SQL databases, git, AWS, Angular, Material Design components and other tools and frameworks. On 2020 I learnt about the blockchain technology, initially as an investor. I ended up being so amazed by the technological opportunities that I quit my job in March 2021. I have been a full time Solidity and DApp developer since November 2021.",
+  "socialLinks": [
     {
-      "UniversityName":"Mumbai University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
-      "Achievements":"Some Achievements"
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/joseramonalonsotapia/",
+      "className": "fa fa-linkedin"
     },
     {
-      "UniversityName":"Some University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
-      "Achievements":"Some Achievements"
+      "name": "github",
+      "url": "http://github.com/josealonso",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "http://skype.com",
+      "className": "fa fa-skype"
     }
   ],
-  "work":[
+  "aboutme": "I finished my B.E. in Electronics Engineering before 2010 and have been working as a Software developer sine then.",
+  "address": "Spain",
+  "website": "https://www.codeclassifiers.com",
+  "education": [
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      "UniversityName": "Mumbai University",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2018",
+      "Achievements": "Some Achievements"
     },
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      "UniversityName": "Some University",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2018",
+      "Achievements": "Some Achievements"
     }
   ],
-  "skillsDescription":"Your skills here",
-  "skills":[
+  "work": [
     {
-      "skillname":"HTML5"
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
     },
     {
-      "skillname":"CSS"
-    },
-    {
-      "skillname":"Reactjs"
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
     }
   ],
-  "portfolio":[
+  "skillsDescription": "Your skills here",
+  "skills": [
     {
-      "name":"project1",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
+      "skillname": "HTML5"
     },
     {
-      "name":"project2",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project.jpg"
+      "skillname": "CSS"
     },
     {
-      "name":"project3",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project2.png"
-    },
-    {
-      "name":"project4",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
+      "skillname": "Reactjs"
     }
   ],
-  "testimonials":[
+  "portfolio": [
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
     },
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
     }
   ]
 }
